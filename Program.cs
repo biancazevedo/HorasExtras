b8c5f9d23e7c65280fc9256c53e9d59a3832510f
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-23 19:41:49 -0300

    f

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿﻿Console.WriteLine("--- Horas Extras ---\n");
+
+Console.WriteLine($"salario-hora {12:C}, {400} horas, sendo {0,4} horas-extra, salário = {Salario(12, 400, 0):C}");
+Console.WriteLine($"salario-hora {12:C}, {400} horas, sendo {20} horas-extra, salário = {Salario(12, 400, 20):C}");
+Console.WriteLine($"salario-hora {24:C}, {400} horas, sendo {0,4} horas-extra, salário = {Salario(24, 400, 0):C}");
+Console.WriteLine($"salario-hora {24:C}, {400} horas, sendo {20} horas-extra, salário = {Salario(24, 400, 20):C}");
+
+double Salario(double salarioHora, double horasTotais, double horasExtras)
+{
+    return (salarioHora * (horasTotais - horasExtras)) + (salarioHora * horasExtras * 1.4);
+}
